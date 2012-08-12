3f72e4bf093002a4f5846eb7acfc04691b7291b5
Sylvain Kalache <sylvain@kalache.fr>
2012-08-12 21:57:58 +0000

    debuging chages + update method

--- lib/proxy.rb
+++ lib/proxy.rb
@@ -1,11 +1,25 @@
 module Huoqiang
   class Proxy
+    def initialize()
+      @logger = Logger.new(File.join(File.dirname(__FILE__),'../logs/collector.log'))
+    end
+
     # Delete a proxy from MongoDB.
     #
     # @param [String] Proxy ip address.
     def delete(ip_address)
       mongo = Mongodb.new
-      mongo.remove({:server_ip => ip_address})
+      mongo.remove({"server_ip" => ip_address})
+    end
+
+    # Update a proxy entry
+    #
+    # @param [String] IP address of the proxy
+    # @param [Hash] Containing new proxy information
+    def update(ip_address, proxy_data)
+      mongo = Mongodb.new
+      proxy_data.delete('_id') # We can't update the "_id" field.
+      mongo.update({"server_ip" => ip_address}, proxy_data)
     end
 
     # Return the best proxy (based on lantency).
@@ -14,7 +28,7 @@ module Huoqiang
     # @return [Array] Array of hashes.
     def get(number = 1)
       mongo = Mongodb.new
-      result = mongo.find({ "latency" => {"$exists" => true}}).sort({ "latency" => 1}).limit(number)
+      result = mongo.find({ "latency" => {"$exists" => true}}).sort({"latency" => 1}).limit(number)
 
       entries = []
       result.each {|entry| ip << entry}
@@ -28,30 +42,41 @@ module Huoqiang
     # @param [Integer] Port of the proxy.
     # @param [Integer] Time after the HTTP request via curl will timeout.
     # @return [Boolean] Proxy working or not.
-    def is_working(proxy_address, proxy_port, timeout=5)
+    def is_working(proxy_address, proxy_port, timeout=3)
       # We are assuming that weibo.com is up
-      c = Curl::Easy.new('http://www.weibo.com/')
+      url = "http://www.weibo.com/"
+      @logger.debug "Will pars #{url}"
+      c = Curl::Easy.new(url)
       c.proxy_url = proxy_address
       c.proxy_port = proxy_port
+      #      c.timeout = 2
+      @logger.debug "Curl initialisation done"
 
       begin
         Timeout::timeout(timeout) do
           c.perform
         end
-
+        @logger.debug "perform passed"
         #We are assuming that Weibo page should contain the keyword weibo
-        if c.body_str.include? 'weibo'
+        if ! c.body_str.nil? and c.body_str.include? 'weibo'
           return true
         else
           return false
         end
+        @logger.debug "include passed"
 
-      rescue Curl::Err::ConnectionFailedError, Curl::Err::ProxyResolutionError, Timeout::Error, Curl::Err::GotNothingError => e
-        @proxy ||= Proxy.new()
-        @proxy.delete(proxy_address)
+      rescue Curl::Err::ConnectionFailedError, Curl::Err::ProxyResolutionError, Timeout::Error,
+        Curl::Err::GotNothingError, Curl::Err::HostResolutionError, Curl::Err::HostResolutionError => e
+        @logger.error "Tried to reach #{url} with #{proxy_address}: #{e.message}"
         return false
+      rescue StandardError => e
+        @logger.error "Tried to reach #{url} with #{proxy_address}: #{e.message}"
+      ensure
+        @logger.debug "YAY"
+        c.close()
       end
-    end
+
+    end # End is_working
 
   end
 end
