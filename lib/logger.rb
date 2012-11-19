0a66e882b2045789a9fc7c0ed7bccd8568341f7a
Sylvain Kalache <sylvain@kalache.fr>
2012-11-19 04:44:26 +0000

    path thing

--- lib/logger.rb
+++ lib/logger.rb
@@ -4,6 +4,6 @@ module Huoqiang
     #
     # @param [String] file_name Name of the log file
     def self.logger(file_name)
-        @logger ||= Logger.new(File.join(File.dirname(__FILE__),"../log/#{file_name}.log"))
+        @logger ||= Logger.new(File.join(File.expand_path(File.dirname(__FILE__)),"../log/#{file_name}.log"))
     end
 end
