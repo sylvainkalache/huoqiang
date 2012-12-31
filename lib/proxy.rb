24d955aafcfd0bd9b9ce68d3770dee16d851bfc9
Sylvain Kalache <sylvain@kalache.fr>
2012-12-31 03:13:46 +0000

    method to check if a proxy is trustable and change methods to self

--- lib/proxy.rb
+++ lib/proxy.rb
@@ -1,5 +1,6 @@
 require 'curb'
 require File.join(File.expand_path(File.dirname(__FILE__)),'mongodb.rb')
+require File.join(File.expand_path(File.dirname(__FILE__)),'http.rb')
 
 module Huoqiang
   class Proxy
@@ -7,7 +8,7 @@ module Huoqiang
     # Delete a proxy from MongoDB.
     #
     # @param [String] Proxy ip address.
-    def delete(ip_address)
+    def self.delete(ip_address)
       mongo = Mongodb.new
       mongo.remove({"server_ip" => ip_address})
     end
@@ -16,7 +17,7 @@ module Huoqiang
     #
     # @param [String] IP address of the proxy
     # @param [Hash] Containing new proxy information
-    def update(ip_address, proxy_data)
+    def self.update(ip_address, proxy_data)
       mongo = Mongodb.new
       proxy_data.delete('_id') # We can't update the "_id" field.
       mongo.update({"server_ip" => ip_address}, proxy_data)
@@ -28,7 +29,7 @@ module Huoqiang
     # @param [Integer] skip Number of entries to skip
     #
     # @return [Array] Array of hashes.
-    def get(limit = 1, skip = 0)
+    def self.get(limit = 1, skip = 0)
       mongo = Mongodb.new
       result = mongo.find({ "latency" => {"$exists" => true}, "unavailable" => false }).sort({"latency" => 1}).limit(limit).skip(skip)
 
@@ -44,13 +45,12 @@ module Huoqiang
     # Set the proxy to an unavailable state
     #
     # @param [String] Proxy ip address
-    def unavailable(ip)
+    def self.unavailable(ip)
       mongo = Mongodb.new()
       proxy_data = mongo.find_one({"server_ip" => ip})
       proxy_data['unavailable'] = true
 
-      proxy = Proxy.new()
-      proxy.update(ip, proxy_data)
+      Proxy.update(ip, proxy_data)
     end
 
     # Test if the proxy is working
@@ -58,8 +58,9 @@ module Huoqiang
     # @param [String] IP address of the proxy.
     # @param [Integer] Port of the proxy.
     # @param [Integer] Time after the HTTP request via curl will timeout.
+    #
     # @return [Boolean] Proxy working or not.
-    def is_working(proxy_address, proxy_port, timeout=5)
+    def self.is_working(proxy_address, proxy_port, timeout=5)
       # We are assuming that weibo.com is up
       url = "http://www.weibo.com/"
       c = Curl::Easy.new(url)
@@ -85,6 +86,42 @@ module Huoqiang
       end
 
     end # End is_working
+
+    # Test if the really process any request
+    # I've noticed that some proxies deny any website
+    # which is not Chinese based
+    # @param [String] IP address of the proxy.
+    # @param [Integer] Port of the proxy.
+    # @param [Integer] Time after the HTTP request via curl will timeout.
+    #
+    # @return [Boolean] Proxy working or not.
+    def self.is_trustable(proxy_address, proxy_port, timeout = 5)
+      @logger = Huoqiang.logger('crawler')
+      # Fair example of a website that should not be censured
+      # and should have a pretty good uptime
+      url = "http://www.nasa.com/"
+
+      proxy_response_code = Http.get_response_code(url, proxy_address, proxy_port)
+
+      c = Curl::Easy.new(url)
+      begin
+        Timeout::timeout(timeout) do
+          c.perform
+        end
+      rescue StandardError => e
+        # TODO
+        # Something went wrong ....
+        @logger.error "[Proxy]Could not query #{url} #{e.message}"
+      end
+
+      # We are supposed to get the same response code
+      # If not, this proxy is probable not trustable
+      if c.response_code != proxy_response_code
+        return false
+      end
+
+      return true
+    end # End is_trustable
 
   end
 end
