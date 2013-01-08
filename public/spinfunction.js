4e53f1b771737113e0416a230d5cc6ee3a4eea5c
Sylvain Kalache <sylvain@kalache.fr>
2013-01-08 06:35:22 +0000

    spinning and form validation

--- public/spinfunction.js
+++ public/spinfunction.js
@@ -1,12 +1,12 @@
 var opts = {
-	lines: 17, // The number of lines to draw
+	lines: 10, // The number of lines to draw
 	length: 0, // The length of each line
-	width: 20, // The line thickness
-	radius: 13, // The radius of the inner circle
+	width: 9, // The line thickness
+	radius: 30, // The radius of the inner circle
 	rotate: 0, // The rotation offset
-	color: '#F3BC15', // #rgb or #rrggbb
-	speed: 1, // Rounds per second
-	trail: 10, // Afterglow percentage
+	color: '#fff', // #rgb or #rrggbb
+	speed: 1.2, // Rounds per second
+	trail: 100, // Afterglow percentage
 	shadow: false, // Whether to render a shadow
 	hwaccel: false, // Whether to use hardware acceleration
 	className: 'spinner', // The CSS class to assign to the spinner
@@ -20,7 +20,22 @@ function spin_stop()
 {
 	spinner.stop();
 }
+
+function sleep(milliSeconds){
+	var startTime = new Date().getTime(); // get the current time
+	while (new Date().getTime() < startTime + milliSeconds); // hog cpu
+}
+
 function spin_start()
 {
-	spinner.spin(target);
+	var x=document.forms["mainForm"]["url"].value;
+	if (x==null || x=="")
+	{
+ 		document.getElementById("address-field").className = "address-wrong";
+		return false;
+	}
+	else
+	{
+		spinner.spin(target);
+	}
 }
